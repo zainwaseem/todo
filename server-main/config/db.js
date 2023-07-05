8343f0e07a91c0f87515f6c363062dfede785632
zainwaseem <zainwaseem9371@gmail.com>
2023-07-05 12:27:02 +0500

    to do web app

---
+++ server-main/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://omidmoaddab:impTq44pugOXwOnt@cluster0.omwsrnz.mongodb.net/"
+    )
+    .then(() => console.log(`Datebase Connected`))
+    .catch((error) => console.log(error.message));
+};
+export default connectDB;
+// zainwaseem9371;
+// fUYHbAFlPTbkKz6i;
